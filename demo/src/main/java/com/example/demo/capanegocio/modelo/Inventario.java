63aa85e855a2562f2c1e0318d4ebdb2ca5a93753
Valente <valentecardenas30@gmail.com>
2025-03-27 08:41:44 -0600

    Agregué entidadea al package modelo

---
+++ demo/src/main/java/com/example/demo/capanegocio/modelo/Inventario.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.capanegocio.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/**
+ *
+ * @author 100077515
+ */
+@Entity
+public class Inventario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idInventario;
+
+    @ManyToOne
+    @JoinColumn(name = "idLibro", nullable = false)
+    private Libro libro;
+
+    @ManyToOne
+    @JoinColumn(name = "idSucursal", nullable = false)
+    private Suscursal sucursal;
+
+    @Column(nullable = false)
+    private int cantidad;
+
+    public int getIdInventario() {
+        return idInventario;
+    }
+
+    public void setIdInventario(int idInventario) {
+        this.idInventario = idInventario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public Suscursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(Suscursal sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Inventario [idInventario=" + idInventario + ", libro=" + libro + ", sucursal=" + sucursal
+                + ", cantidad=" + cantidad + "]";
+    }
+
+    
+
+}
---
+++ demo/src/main/java/com/example/demo/capanegocio/modelo/Reporte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.capanegocio.modelo;
+
+import jakarta.persistence.Entity;
+
+/**
+ *
+ * @author 100077515
+ */
+@Entity
+public class Reporte {
+    
+}
